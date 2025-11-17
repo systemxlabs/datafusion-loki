a6f7850187aed4c2500dfd282debde111054ee05
张林伟 <lewiszlw520@gmail.com>
2025-11-17 15:58:43 +0800

    Add label and line filter tests

--- integration-tests/testdata/init.sql
+++ integration-tests/testdata/init.sql
@@ -1,1 +1,2 @@
-INSERT INTO loki values (current_timestamp(), Map{'app': 'my-app'}, 'this is a test log');
+INSERT INTO loki values (current_timestamp(), Map{'app': 'my-app1'}, 'this is aaa log');
+INSERT INTO loki values (current_timestamp(), Map{'app': 'my-app2'}, 'this is bbb log');
--- integration-tests/tests/table.rs
+++ integration-tests/tests/table.rs
@@ -4,11 +4,12 @@ use integration_tests::assert_loki_output;
 async fn full_table_scan() -> Result<(), Box<dyn std::error::Error>> {
     assert_loki_output(
         "select * from loki",
-        r#"+--------------------------------------------------------------+--------------------+
-| labels                                                       | line               |
-+--------------------------------------------------------------+--------------------+
-| {app: my-app, detected_level: unknown, service_name: my-app} | this is a test log |
-+--------------------------------------------------------------+--------------------+"#,
+        r#"+----------------------------------------------------------------+-----------------+
+| labels                                                         | line            |
++----------------------------------------------------------------+-----------------+
+| {app: my-app1, detected_level: unknown, service_name: my-app1} | this is aaa log |
+| {app: my-app2, detected_level: unknown, service_name: my-app2} | this is bbb log |
++----------------------------------------------------------------+-----------------+"#,
     )
     .await?;
     Ok(())
@@ -21,10 +22,42 @@ async fn timestamp_filter() -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::test]
 async fn label_filter() -> Result<(), Box<dyn std::error::Error>> {
+    assert_loki_output(
+        "select * from loki where map_get(labels, 'app') = 'my-app2'",
+        r#"+----------------------------------------------------------------+-----------------+
+| labels                                                         | line            |
++----------------------------------------------------------------+-----------------+
+| {app: my-app2, detected_level: unknown, service_name: my-app2} | this is bbb log |
++----------------------------------------------------------------+-----------------+"#,
+    )
+    .await?;
+
+    assert_loki_output(
+        "select * from loki where map_get(labels, 'app') = 'not-existing'",
+        r#"++
+++"#,
+    )
+    .await?;
     Ok(())
 }
 
 #[tokio::test]
 async fn line_filter() -> Result<(), Box<dyn std::error::Error>> {
+    assert_loki_output(
+        "select * from loki where line like '%bbb%'",
+        r#"+----------------------------------------------------------------+-----------------+
+| labels                                                         | line            |
++----------------------------------------------------------------+-----------------+
+| {app: my-app2, detected_level: unknown, service_name: my-app2} | this is bbb log |
++----------------------------------------------------------------+-----------------+"#,
+    )
+    .await?;
+
+    assert_loki_output(
+        "select * from loki where line like '%not-existing%'",
+        r#"++
+++"#,
+    )
+    .await?;
     Ok(())
 }
